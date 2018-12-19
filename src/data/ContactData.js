00feb282aa5b007fd03478cf492182e2cb180bbc
shahW <sw@nmbl.sg>
2018-12-19 18:32:22 +0800

    Added dummy data

---
+++ src/data/ContactData.js
@@ -0,0 +1,32 @@
+const contactData = [
+	{
+		id: 1,
+		img: "http://placekitten.com/300/200?image=1",
+		name: "Mr. Kitten X",
+		phone: "1234556677",
+		email: "name@email.com"
+	},
+	{
+		id: 2,
+		img: "http://placekitten.com/300/200?image=2",
+		name: "Snuffle nose",
+		phone: "1234556677",
+		email: "nosey@email.com"
+	},
+	{
+		id: 3,
+		img: "http://placekitten.com/300/200?image=3",
+		name: "Xavier",
+		phone: "123-455-66-77",
+		email: "xamvier@email.com"
+	},
+	{
+		id: 4,
+		img: "http://placekitten.com/300/200?image=4",
+		name: "Bobby Brown",
+		phone: "88923-455-66-77",
+		email: "bobby@email.com"
+	}
+]
+
+export default contactData
---
+++ src/data/JokesData.js
@@ -0,0 +1,36 @@
+const jokesData = [
+	{
+		id: 1,
+		q: "",
+		a:
+			"It’s hard to explain puns to kleptomaniacs because they always take things literally."
+	},
+	{
+		id: 2,
+		q: "What's the best thing about Switzerland?",
+		a: "I don't know, but the flag is a big plus!"
+	},
+	{
+		id: 3,
+		q:
+			"Did you hear about the mathematician who's afraid of negative numbers?",
+		a: "He'll stop at nothing to avoid them!"
+	},
+	{
+		id: 4,
+		q: "Hear about the new restaurant called Karma?",
+		a: "There’s no menu: You get what you deserve."
+	},
+	{
+		id: 5,
+		q: "Did you hear about the actor who fell through the floorboards?",
+		a: "He was just going through a stage."
+	},
+	{
+		id: 6,
+		q: "Did you hear about the claustrophobic astronaut?",
+		a: "He just needed a little space."
+	}
+]
+
+export default jokesData
