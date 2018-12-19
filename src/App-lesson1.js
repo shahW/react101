6cf8a167d1f1977c4d21961eab0b3b0d836266e5
shahW <sw@nmbl.sg>
2018-12-19 17:33:42 +0800

    structuring the app to include sass

--- src/App-lesson1.js
+++ src/App-lesson1.js
@@ -2,12 +2,14 @@ import React, { Component } from "react"
 import "./scss/App.scss"
 import Header from "./components/common/Header"
 import Footer from "./components/common/Footer"
+import ContentLesson1 from "./components/content-lesson-1"
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
 				<Header />
+				<ContentLesson1 />
 				<Footer />
 			</div>
 		)
---
+++ src/components/content-lesson-1.js
@@ -0,0 +1,5 @@
+import React from "react"
+function ContentLesson1() {
+	return null
+}
+export default ContentLesson1
